1a4a88b0d276d47c8a68fc280ad25e311d09f12c
ParisNeo <aloui.seifeddine@gmail.com>
2023-04-30 03:17:38 +0200

    Upgraded code

--- examples/example_with_pyllamacpp.py
+++ examples/example_with_pyllamacpp.py
@@ -7,9 +7,12 @@ if __name__=="__main__":
 
     personality = AIPersonality("personalities_zoo/english/general/gpt4all_chat_bot")
     model = Model(ggml_model=r'C:\Users\aloui\Documents\ai\GPT4ALL-ui\GPT4All\models\llama_cpp/gpt4all-lora-quantized-ggml.bin',
-                  prompt_context=personality.personality_conditioning,
-                  prompt_prefix=personality.user_message_prefix,
-                  prompt_suffix=personality.ai_message_prefix,
+                  prompt_context=personality.personality_conditioning+
+                  personality.link_text+
+                  personality.ai_message_prefix+
+                  personality.welcome_message,
+                  prompt_prefix=personality.link_text + personality.user_message_prefix + personality.link_text,
+                  prompt_suffix=personality.link_text + personality.ai_message_prefix + personality.link_text,
                   anti_prompts=[personality.user_message_prefix, personality.ai_message_prefix])
     print(personality.welcome_message)
     while True:
