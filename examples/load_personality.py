2e8a59cbf86a813e967199f14f5b4738c0b2029d
ParisNeo <aloui.seifeddine@gmail.com>
2023-05-01 12:29:20 +0200

    bugfix in personalities

--- examples/load_personality.py
+++ examples/load_personality.py
@@ -2,6 +2,6 @@ from pyaipersonality import AIPersonality
 
 
 if __name__=="__main__":
-    personality = AIPersonality("personalities_zoo/english/general/gpt4all_chat_bot")
+    personality = AIPersonality("personalities_zoo/english/general/gpt4all")
     print("Done")
     print(f"{personality}")
